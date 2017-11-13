7984114336015d227f024ba87b3d79437f398f6f
winnie <winnie@winniedeMacBook-Pro.local>
2017-11-13 17:42:45 +0800

    Ver.0.1

---
+++ page_01.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>JQnets 甲寬網路科技</title>
+    <!-- CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/fonts/notosanstc/notosanstc.css" rel="stylesheet">
+    <link href="assets/fonts/font-awesome/font-awesome.css" rel="stylesheet">
+    <link href="assets/css/jqnets.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- JS -->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src='assets/js/fastclick.js'></script>
+    <script type="text/javascript" src="assets/js/jqnets.js"></script>
+  </head>
+  <body>
+        <!-- 公司標誌/選單 -->
+        <nav id="jq-globalnav">
+            <div class="small-size">
+                <img class="jq-logo" src="assets/images/logo.png" onclick="javascript:click_link(0);" alt="JQnets 甲寬網路科技" title="JQnets 甲寬網路科技">
+                <div class="pull-right">
+                    <div class="button_container">
+                        <span class="top"></span>
+                        <span class="middle"></span>
+                        <span class="bottom"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="default-size">
+                <ul class="menu-list">
+                    <li class="menu-item menu-jq-logo">
+                        <a class="menu-link" href="javascript:click_link(0);">
+                            <img class="jq-logo" src="assets/images/logo.png" alt="JQnets 甲寬網路科技" title="JQnets 甲寬網路科技">
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link active" href="javascript:click_link(1);">
+                            <span class="menu-link-text">新創小幫手</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link" href="javascript:click_link(2);">
+                            <span class="menu-link-text">你說我來做</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link" href="javascript:click_link(3);">
+                            <span class="menu-link-text">客製化開發</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link" href="javascript:click_link(4);">
+                            <span class="menu-link-text">活動式網站</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link" href="javascript:click_link(5);">
+                            <span class="menu-link-text">關於甲寬</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="menu-link" href="javascript:click_link(6);">
+                            <span class="menu-link-text">連絡我們</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- /公司標誌/選單 -->
+
+        <!-- 新創小幫手 -->
+        <section class="jq-body">
+            <!-- 輪播說明 -->
+            <div id="page_01_carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#page_01_carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#page_01_carousel" data-slide-to="1" class=""></li>
+                    <li data-target="#page_01_carousel" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="assets/images/img_01_min.jpg" alt="First slide">
+                        <div class="carousel-content">
+                            說明文字1。
+                        </div>
+                        <div class="carousel-caption"></div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/images/img_02_min.jpg" alt="Second slide">
+                        <div class="carousel-content">
+                            說明文字2。
+                        </div>
+                        <div class="carousel-caption"></div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/images/img_03_min.jpg" alt="Third slide">
+                        <div class="carousel-content">
+                            說明文字3。
+                        </div>
+                        <div class="carousel-caption"></div>
+                    </div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#page_01_carousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#page_01_carousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- /輪播說明 -->
+<!-- 子功能選單 -->
+<nav style="border:1px solid;width:100%;height: 100px;"></nav>
+<!-- /子功能選單 -->
+        </section>
+         <!-- /新創小幫手 -->
+
+        <!-- 活動式網站 -->
+        <section class="jq-body">
+            <div class="fill-img img-04"></div>
+            <div class="overlay-wall"></div>
+            <div class="jq-container">
+                <p>這是一段範例文字描述，範例文字描述。範例文字描述，範例文字描述。範例文字描述，範例文字描述。範例文字描述，範例文字描述。</p>
+                <p class="mg-top-60 jq-learn-more-text">進一步了解<a class="jq-learn-more-btn mg-left-10" href="javascript:click_link(4);">活動式網站 <i class="glyphicon glyphicon-chevron-right"></i></a></p>
+            </div>
+        </section>
+         <!-- /活動式網站 -->
+        <!-- 頁尾 -->
+        <section class="jq-footer">
+            <div class="jq-container">
+                <div class="row no-mg">
+                    <!--
+                    <div class="col-sm-4">
+                        <p class="header">甲寬網路科技</p>
+                        <ul class="menu-list">
+                            <li><a href="javascript:click_link(1);">新創小幫手</a></li>
+                            <li><a href="javascript:click_link(2);">你說我來做</a></li>
+                            <li><a href="javascript:click_link(3);">客製化開發</a></li>
+                            <li><a href="javascript:click_link(4);">活動式網站</a></li>
+                            <li><a href="javascript:click_link(5);">關於甲寬</a></li>
+                            <li><a href="javascript:click_link(6);">連絡我們</a></li>
+                        </ul>
+                    </div> -->
+                    <div class="col-sm-5">
+                        <p class="header">關於甲寬</p>
+                        <div class="content">真誠的以「品質至上」、「最高服務」的思維設計，以做為我們成立公司的宗旨。「甲」：代表第一，我們要有衝勁，要做就要做到最好。「寬」：寬廣視野，不要侷現於眼前的利益，要服務全世界，全球化市場。</div>
+                    </div>
+                    <div class="col-sm-5">
+                        <p class="header">連絡我們</p>
+                        <ul class="menu-list">
+                            <li><i class="fa fa-skype fa-fw"></i> gm0621</li>
+                            <li><i class="fa fa-phone fa-fw"></i> 0975-163-960&nbsp;&nbsp;Jimmy</li>
+                            <li><i class="fa fa-envelope fa-fw"></i> jqnets@gmail.com</li>
+                            <li><i class="fa fa-map-marker fa-fw"></i> 404 台中市北區崇德路一段631號A棟6樓之1</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="copynote">© 2015 JQnets Inc. 甲寬網路科技 All rights reserved.</div>
+        </section>
+        <!-- /頁尾 -->
+
+  <script>
+    // $(document).ready(function()
+    // {
+
+    // });
+  </script>
+  </body>
+
+
+</html>
